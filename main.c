6f7b5d279b9f0b7206b8f099a4af1c804899eedf
get <adrien.pusel@gmail.com>
2018-05-19 20:50:34 +0200

    save

--- main.c
+++ main.c
@@ -29,51 +29,84 @@
 //	return (1);
 //}
 
-//void do_seg(int x1, int y1, int x2, int y2, t *param)
-//{
-//	int dx;
-//	int dy;
-//	int e;
-//
-//	e = x2 - x1;
-//	dx = e * 2;
-//	dy = (y2 - y1) * 2;
-//
-//	while (x1 < x2)
-//	{
-//		++x1;
-//		printf("%d %d\n", x1, y1);
-//		mlx_pixel_put(param->M, (param)->widow, x1, y1, 0xFFFFFF);
-//		if (e - dy >= 0)
-//		{
-//			printf("%d \n", dx);
-//			++y1;
-//			e += dx;
-//		}
-//		x2--;
-//	}
-//}
+void do_seg(int x1, int y1, int x2, int y2, t_mlx mlx)
+{
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int e;
+
+
+	ft_printf("%d %d \n", dx, dy);
+	dy = (y1 - y2) * 2;
+	e = dx;
+	dx = e * 2;
+
+	while (x1 < x2)
+	{
+		++x1;
+		fill_pixel(mlx, x1, y1, 0xFFFFFF);
+		if (e -= dy >= 0)
+		{
+			++y1;
+			e += dx;
+		}
+		x2--;
+	}
+}
 
 
 // get les coordoner de la souris
 
-int get_souris_click(int button,int x,int y,void *param)
+int get_souris_click(int button, int x, int y, void *param)
 {
+	static int i = 2;
+	static int x1 = 0;
+	static int x2 = 0;
+	static int y1 = 0;
+	static int y2 = 0;
+	t_mlx mlx = param;
 
+	(void) button;
+	(void) param;
+	(void) x;
+	(void) y;
+	//
+	//	if (i == 0)
+	//	{
+	//		x1 = x;
+	//		y1 = y;
+	//		++i;
+	//	}
+	//
+	//	else if (i == 1)
+	//	{
+	//		x2 = x;
+	//		y2 = y;
+	//		++i;
+	//	}
 
-	ft_printf("%d %d \n", x, y);
+	if (i == 2)
+	{
+		//		do_seg(x1, y1, x2, y2, param);
+		do_seg(150, 300, 500, 150, param);
+		mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img, 0, 0);
+		i = 0;
+	}
+	ft_printf("%d %d %d %d %d \n", i, x1, y1, x2, y2);
 
 	return (TRUE);
 }
-
 
 int main()
 {
 	t_mlx mlx;
 
 	mlx = new_mlx(1000, 1000, "lala");
-	mlx_mouse_hook(mlx->window, get_souris_click, mlx);
 
+	do_seg(150, 150, 500, 300, mlx);
+	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img, 0, 0);
+
+	mlx_mouse_hook(mlx->window, get_souris_click, mlx);
 
 	mlx_loop(mlx->mlx);
 	return 0;
